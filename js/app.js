a62ed96ed4d7f0cc93d636b7ddfe7a0352fb914a
Joseph Sayad <JosephSayad95@gmail.com>
2016-01-05 18:58:01 -0500

    Functionality implemented

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 function runApplication() {
   $('.input-form').on('click', 'button', function(event) {
     event.preventDefault();
+    $('.data-display').html('');
     var userName = null, artistName = null;
 
     userName = $('.user-name').val();
@@ -27,7 +28,7 @@ function getNewArtists(content) {
     type: 'music',
     info: 1, 
     k: '191288-APIHack-815HRZIG',
-    limit: 6
+    limit: 10
   };
   $.ajax({
     url: 'https://www.tastekid.com/api/similar',
@@ -36,18 +37,18 @@ function getNewArtists(content) {
     type: 'GET',
   })
   .done(function(result) {
-    console.log(result);
     var counter = 0;
     $.each(result.Similar.Results, function(i, item) {
-      var artistNumber = new Array('One', 'Two', 'Three', 'Four', 'Five', 'Six');
-      var newArtist = showArtist(item.Name, artistNumber[counter]);
+      var artistNumber = new Array('One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten');
+      console.log(item);
+      var newArtist = showArtist(item.Name, artistNumber[counter], item.wTeaser, item.yUrl);
       $('.data-display').append(newArtist);
       counter++;
     });
   });
 }
 
-function showArtist(artistName, artistNumber) {
-  var result = ('<div class="featured-artist template col-xs-10 col-xs-offset-1 well-lg"><div class="row"><div class="col-xs-12"><h2 class="artist-header lead">Artist'+ artistNumber +':<span> '+ artistName +'</span></h2></div></div></div>');
+function showArtist(artistName, artistNumber, artistInfo, youtubeContent) {
+  var result = ('<div class="featured-artist template col-xs-10 col-xs-offset-1 well-lg"><div class="row"><div class="col-xs-12"><h2 class="artist-header lead">Artist'+ artistNumber +':<span> '+ artistName +'</span></h2></div></div><div class="row"><div class="col-xs-12 teaser"><p class="col-xs-8 text-justify">'+ artistInfo +'</p></div><div class="col-xs-12 teaser youtube"><p class="col-xs-8 text-justify"><a href="'+ youtubeContent +'" target="_blank">Check This Artist Out!</a></p></div></div></div>');
   return result;
 }
